849ffe7452561b584befc0f15d78eb375789d928
dev-hyeonmin <dev.hyeonmin@gmail.com>
2024-03-05 18:42:56 +0900

    client & server fetch / loading

--- app/(movies)/movies/page.tsx
+++ app/(movies)/movies/page.tsx
@@ -1,3 +1,34 @@
-export default function Movie() {
-    return <h1>Movie</h1>;
+'use client';
+
+// client side data fetching
+// export default function Movie() {
+//     const [isLoading, setIsLoading] = useState(true);
+//     const [movies, setMovies] = useState([]);
+//     const getMovies = async () => {
+//         const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies")
+//         const json = await response.json();
+        
+//         setMovies(json);
+//         setIsLoading(false);
+//     };
+
+//     useEffect(() => {
+//         getMovies();
+//     }, [])
+
+//     return <div>{isLoading ? 'Loading...' : JSON.stringify(movies)}</div>;
+// }
+
+// server side data fetching
+const URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
+
+async function getMovies () {
+    const reponse = await fetch(URL);
+    const json = await reponse.json();
+    return json;
+}
+
+export default async function Movie() {
+    const movies = await getMovies();
+    return <div>{JSON.stringify(movies)}</div>;
 }
